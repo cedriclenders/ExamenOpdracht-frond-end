6540d2e501f8d2a61b68fda2e63c413b150d715d
Cedric Lenders <cedric.lenders@student.kdg.be>
2021-06-02 00:06:04 +0200

    fixed navbar en animation

---
+++ src/js/flyout.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".mobileIcon").click(function () {
+      $(this).toggleClass("active");
+      $(".navmobile").toggleClass("hidden");
+    });
+  
+    $(".fa-times-circle").click(function () {
+      $(this).toggleClass("active");
+      $(".navmobile").toggleClass("hidden");
+    });
+  });
